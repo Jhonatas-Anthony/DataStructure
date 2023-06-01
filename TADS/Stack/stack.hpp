b05079ab70d13e1ca4c0861304efd0ee5d920772
Jhonatas-Anthony <jhonatasanthony6@gmail.com>
2023-05-31 23:31:39 -0300

    criação do arquivo

---
+++ TADS/Stack/stack.hpp
@@ -0,0 +1,19 @@
+/*
+Title: stack.hpp
+Author: Jhonatas Anthony Dantas Araújo
+Date: 30/05/2023
+Last Update: 31/05/2023
+*/
+
+#ifndef STACK_HPP
+#define STACK_HPP
+
+class Stack{
+    public:
+
+    private:
+
+
+};
+
+#endif
